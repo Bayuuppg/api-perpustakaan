a253eb911c4089b9bc33a272aad5a634089ed5a7
BAYU PRATAMA <inipunyabayu@gmail.com>
2023-02-03 14:55:51 +0700

    praktek show api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SiswaC;
 
 Route::get('/', function () {
      dd('Hello World APi !');
 });
+
+Route::get('/siswa', [SiswaC::class, 'index']);
